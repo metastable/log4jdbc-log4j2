b74e98feaa1edfc5fa7a4b6f5d20d95e5c1f547e
Felipe Lima <felipe.lima@gmail.com>
2018-01-17 10:45:44 -0800

    Fix NPE in DefaultResultSetCollector

--- log4jdbc-log4j2-jdbc4.1/src/main/java/net/sf/log4jdbc/sql/resultsetcollector/DefaultResultSetCollector.java
+++ log4jdbc-log4j2-jdbc4.1/src/main/java/net/sf/log4jdbc/sql/resultsetcollector/DefaultResultSetCollector.java
@@ -40,65 +40,65 @@ public class DefaultResultSetCollector implements ResultSetCollector {
       this.lastValueReturnedByNext = true;
   }
   /**
-   * A {@code boolean} defining whether parameters of this {@code DefaultResultSetColector} 
+   * A {@code boolean} defining whether parameters of this {@code DefaultResultSetColector}
    * has already been obtained from a {@code ResultSetMetaData}.
    */
   private boolean loaded;
   /**
-   * An <code>int</code> representing the number of columns 
-   * in the real <code>ResultSet</code> object, obtained by calling 
+   * An <code>int</code> representing the number of columns
+   * in the real <code>ResultSet</code> object, obtained by calling
    * <code>getColumnCount</code> on the related <code>ResultSetMetaData</code> object.
-   * This attribute is an <code>Integer</code> rather than an <code>int</code>, 
-   * so that we can distinguish the case where the column count has not yet been obtained, 
+   * This attribute is an <code>Integer</code> rather than an <code>int</code>,
+   * so that we can distinguish the case where the column count has not yet been obtained,
    * from the case when the column count is 0 (should never happend, but still...)
    */
   private int columnCount;
   /**
-   * A <code>Map</code> where the key is an <code>Integer</code> representing 
-   * the index of a column, and the corresponding value is a <code>String</code> 
-   * being the label of the column. 
-   * This <code>Map</code> is populated by calling <code>getColumnLabel</code> 
-   * on the <code>ResultSetMetaData</code> object of the real <code>ResultSet</code> object, 
+   * A <code>Map</code> where the key is an <code>Integer</code> representing
+   * the index of a column, and the corresponding value is a <code>String</code>
+   * being the label of the column.
+   * This <code>Map</code> is populated by calling <code>getColumnLabel</code>
+   * on the <code>ResultSetMetaData</code> object of the real <code>ResultSet</code> object,
    * for each column (see {@link #columnCount}).
    */
   private Map<Integer, String> columnLabels;
   /**
-   * A <code>Map</code> where the key is an <code>Integer</code> representing 
-   * the index of a column, and the corresponding value is a <code>String</code> 
-   * being the name of the column. 
-   * This <code>Map</code> is populated by calling <code>getColumnLabel</code> 
-   * on the <code>ResultSetMetaData</code> object of the real <code>ResultSet</code> object, 
+   * A <code>Map</code> where the key is an <code>Integer</code> representing
+   * the index of a column, and the corresponding value is a <code>String</code>
+   * being the name of the column.
+   * This <code>Map</code> is populated by calling <code>getColumnLabel</code>
+   * on the <code>ResultSetMetaData</code> object of the real <code>ResultSet</code> object,
    * for each column (see {@link #columnCount}).
    */
   private Map<Integer, String> columnNames;
   /**
-   * A <code>boolean</code> that is the last value returned by a call 
-   * to <code>next</code> (or <code>first</code>) on the related <code>ResultSet</code>. 
-   * This allows to know if this <code>ResultSetCollector</code> must be printed 
-   * when <code>close</code> is called on the related <code>ResultSet</code>: 
-   * if the previous call to <code>next</code> returned <code>true</code>, 
-   * then this <code>ResultSetCollector</code> must be printed 
-   * on the call to <code>close</code>; otherwise, it was already printed following 
-   * the previous call to <code>next</code>, and should not be printed 
+   * A <code>boolean</code> that is the last value returned by a call
+   * to <code>next</code> (or <code>first</code>) on the related <code>ResultSet</code>.
+   * This allows to know if this <code>ResultSetCollector</code> must be printed
+   * when <code>close</code> is called on the related <code>ResultSet</code>:
+   * if the previous call to <code>next</code> returned <code>true</code>,
+   * then this <code>ResultSetCollector</code> must be printed
+   * on the call to <code>close</code>; otherwise, it was already printed following
+   * the previous call to <code>next</code>, and should not be printed
    * on the call to <code>close</code>.
    * <p>
-   * This attribute must not be reset after a printing (the other attributes are, through a call 
-   * to <code>reset</code>), in case the related <code>ResultSet</code> is reused. 
+   * This attribute must not be reset after a printing (the other attributes are, through a call
+   * to <code>reset</code>), in case the related <code>ResultSet</code> is reused.
    * <p>
-   * Default value at initialization is <code>true</code> (<code>ResultSetCollector</code> 
+   * Default value at initialization is <code>true</code> (<code>ResultSetCollector</code>
    * not printed).
    */
   private boolean lastValueReturnedByNext;
   private List<Object> row;
   private List<List<Object>> rows;
   private Map<String, Integer> colNameToColIndex;
-  private int colIndex; 
+  private int colIndex;
   //same getters in JDBC 4.2
-  private static final List<String> GETTERS = Arrays.asList(new String[] {"getArray", 
-          "getAsciiStream", "getBigDecimal", "getBinaryStream", "getBlob", "getBoolean", 
-          "getByte", "getBytes", "getCharacterStream", "getClob", "getDate", "getDouble", 
-          "getFloat", "getInt", "getLong", "getNCharacterStream", "getNClob", "getNString", 
-          "getObject", "getRef", "getRowId", "getShort", "getSQLXML", "getString",  
+  private static final List<String> GETTERS = Arrays.asList(new String[] {"getArray",
+          "getAsciiStream", "getBigDecimal", "getBinaryStream", "getBlob", "getBoolean",
+          "getByte", "getBytes", "getCharacterStream", "getClob", "getDate", "getDouble",
+          "getFloat", "getInt", "getLong", "getNCharacterStream", "getNClob", "getNString",
+          "getObject", "getRef", "getRowId", "getShort", "getSQLXML", "getString",
           "getTime", "getTimestamp", "getUnicodeStream", "getURL"});
 
   public List<List<Object>> getRows() {
@@ -126,8 +126,8 @@ public class DefaultResultSetCollector implements ResultSetCollector {
 	  if (this.loaded) {
 		  return;
 	  }
-	  //otherwise, get all data now, so that we don't need 
-	  //to use the ResultSetMetaData later (with some drivers, 
+	  //otherwise, get all data now, so that we don't need
+	  //to use the ResultSetMetaData later (with some drivers,
 	  //it cannot be used once the ResultSet has been closed)
       try {
     	  if (!rs.isClosed()) {
@@ -138,12 +138,12 @@ public class DefaultResultSetCollector implements ResultSetCollector {
       }
       this.loaded = true;
   }
-  
+
   /**
-   * Perform the operations requested by {@link #loadMetaDataIfNeeded(ResultSet)}, 
+   * Perform the operations requested by {@link #loadMetaDataIfNeeded(ResultSet)},
    * using directly the related <code>ResultSetMetaData</code>.
-   * @param metaData 	The <code>ResultSetMetaData</code> through which the information 
-   * 					needed by {@link #loadMetaDataIfNeeded(ResultSet)} are obtained. 
+   * @param metaData 	The <code>ResultSetMetaData</code> through which the information
+   * 					needed by {@link #loadMetaDataIfNeeded(ResultSet)} are obtained.
    */
   private void loadMetaDataIfNeeded(ResultSetMetaData metaData)
   {
@@ -151,8 +151,8 @@ public class DefaultResultSetCollector implements ResultSetCollector {
 	  if (this.loaded) {
 		  return;
 	  }
-	  //otherwise, get all data now, so that we don't need 
-	  //to use the ResultSetMetaData later (with some drivers, 
+	  //otherwise, get all data now, so that we don't need
+	  //to use the ResultSetMetaData later (with some drivers,
 	  //it cannot be used once the ResultSet has been closed)
       try {
           if (metaData == null) {
@@ -168,13 +168,16 @@ public class DefaultResultSetCollector implements ResultSetCollector {
     		  this.columnNames.put(column, name);
     		  colNameToColIndex.put(label, column);
     		  colNameToColIndex.put(name, column);
-    		  //get also the table name to resolve calls such as: 
+    		  //get also the table name to resolve calls such as:
     		  //rs.getString("myTable.myColumn")
-              String table = metaData.getTableName(column).toLowerCase();
+            String tableName = metaData.getTableName(column);
+            if (tableName != null) {
+              String table = tableName.toLowerCase();
               colNameToColIndex.put(table + "." + name, column);
-              //not sure whether table name can be mixed with column label, 
+              //not sure whether table name can be mixed with column label,
               //but just in case...
               colNameToColIndex.put(table + "." + label, column);
+            }
     	  }
     	  this.loaded = true;
       } catch (SQLException e) {
@@ -185,25 +188,25 @@ public class DefaultResultSetCollector implements ResultSetCollector {
   public String getColumnName(int column) {
       return this.columnNames.get(column);
   }
-  
+
   public String getColumnLabel(int column) {
 	  return this.columnLabels.get(column);
   }
 
   /*
    * (non-Javadoc)
-   * 
+   *
    * @see net.sf.log4jdbc.ResultSetCollector#methodReturned(net.sf.log4jdbc.
    * ResultSetSpy, java.lang.String, java.lang.Object, java.lang.Object,
    * java.lang.Object)
    */
   @Override
-  public boolean methodReturned(ResultSetSpy resultSetSpy, String methodCall, Object returnValue, 
-		  Object targetObject, Object... methodParams) 
+  public boolean methodReturned(ResultSetSpy resultSetSpy, String methodCall, Object returnValue,
+		  Object targetObject, Object... methodParams)
   {
-         
+
     if (methodCall.startsWith("get") && methodParams != null && methodParams.length == 1) {
-            
+
       String methodName = methodCall.substring(0, methodCall.indexOf('('));
       if (GETTERS.contains(methodName) && getColumnCount() != 0) {
         setColIndexFromGetXXXMethodParams(methodParams);
@@ -219,13 +222,13 @@ public class DefaultResultSetCollector implements ResultSetCollector {
     if ("next()".equals(methodCall) || "first()".equals(methodCall)) {
     	this.lastValueReturnedByNext = (Boolean) returnValue;
     }
-    if ("next()".equals(methodCall) || "first()".equals(methodCall) || 
+    if ("next()".equals(methodCall) || "first()".equals(methodCall) ||
     		"close()".equals(methodCall)) {
       loadMetaDataIfNeeded(resultSetSpy.getRealResultSet());
-      boolean isEndOfResultSet = 
-    		  //"close" triggers a printing only if the previous call to next 
+      boolean isEndOfResultSet =
+    		  //"close" triggers a printing only if the previous call to next
     		  //did not return false (end of result set already reached)
-    		  ("close()".equals(methodCall) && this.lastValueReturnedByNext != false) || 
+    		  ("close()".equals(methodCall) && this.lastValueReturnedByNext != false) ||
     		  Boolean.FALSE.equals(returnValue) ;
       if (row != null) {
         if (rows == null)
@@ -241,7 +244,7 @@ public class DefaultResultSetCollector implements ResultSetCollector {
 
     if ("getMetaData()".equals(methodCall)) {
       // If the client code calls getMetaData then we don't have to
-      //here we assume that the real ResultSet is not yet closed. 
+      //here we assume that the real ResultSet is not yet closed.
       this.loadMetaDataIfNeeded((ResultSetMetaData) returnValue);
     }
     return false;
@@ -280,7 +283,7 @@ public class DefaultResultSetCollector implements ResultSetCollector {
 
   @Override
   public void preMethod(ResultSetSpy resultSetSpy, String methodCall, Object... methodParams) {
-    if ((methodCall.equals("next()") || methodCall.equals("close()")) && 
+    if ((methodCall.equals("next()") || methodCall.equals("close()")) &&
     		fillInUnreadValues) {
       if (row != null) {
         int colIndex = 0;
@@ -288,7 +291,7 @@ public class DefaultResultSetCollector implements ResultSetCollector {
           if (v != null && v.toString().equals(UNREAD)) {
             Object resultSetValue = null;
             try {
-              // Fill in any unread data 
+              // Fill in any unread data
               resultSetValue = JdbcUtils.getResultSetValue(resultSetSpy.getRealResultSet(),colIndex+1);
             } catch (SQLException e) {
               resultSetValue = UNREAD_ERROR;
